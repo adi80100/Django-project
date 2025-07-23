97925ca0d977de700cae4cdabfb5dad2a67b7d07
Adii Dhore <adityadhore80100@gmail.com>
2025-07-23 14:28:50 +0530

    Initial commit

---
+++ Desktop/contactBook/contactbook/contacts/views.py
@@ -0,0 +1,30 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
+def contact_create(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+def contact_update(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    form = ContactForm(request.POST or None, instance=contact)
+    if form.is_valid():
+        form.save()
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+def contact_delete(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_confirm_delete.html', {'contact': contact})
